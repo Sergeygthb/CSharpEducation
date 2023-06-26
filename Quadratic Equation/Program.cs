30d77d21fb1a3515122e90443f2c7a24d431785e
Sergey Suhodoev <walllkman@mail.ru>
2023-06-26 21:56:26 +0400

    Допилил алгоритм

--- Quadratic Equation/Program.cs
+++ Quadratic Equation/Program.cs
@@ -3,8 +3,8 @@
     class Program
     {
         static void Main(string[] args)
-        {
-            double a, b, c;
+        {   
+            double a, b, c, x1, x2;
             Console.WriteLine("ax^2 + bx + c = 0, при a != 0");
             Console.WriteLine("Введите a: ");
             a = Convert.ToDouble(Console.ReadLine());
@@ -12,30 +12,36 @@
             b = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите c: ");
             c = Convert.ToDouble(Console.ReadLine());
+            
+            double d = GetDiscriminant(a, b, c);
 
-            new Reshenie(a, b, c).raschet();
-            Console.ReadKey();
-        }
-    }
-    class Reshenie
-    {
-        private double a;
-        private double b;
-        private double c;
-        private double x1;
-        private double x2;
+            if (d > 0)
+            {
+                x1 = (-b + Math.Pow(d, 0.5)) / (2 * a);
+                x2 = (-b - Math.Pow(d, 0.5)) / (2 * a);
+                Console.WriteLine($"x1 = {x1}, x2 = {x2}");
+            }
+            else if (d == 0)
+            {
+                x1 = -b / (2 * a);
+                Console.WriteLine($"D = 0, корень один x = {x1}");
+            }
+            else  
+            {
+                Console.WriteLine("Дискриминант меньше 0, корней нет");
+            }
+            //  Console.ReadKey();
 
-        public Reshenie(double a, double b, double c)
-        {
-            this.a = a;
-            this.b = b;
-            this.c = c;
         }
-        public void raschet()
+        static double GetDiscriminant(double a, double b, double c)
         {
-            D = (Math.Pow(a, 5) - Math.Pow(a, 3) * Math.Sqrt(b)) / (2 * c);
-            Console.WriteLine("Ответ: {0}", D);
-            Console.ReadKey();
+            return Math.Pow(b, 2) - 4 * a * c;
+        }     
+        static bool MakeTest(double a, double b, double c)
+        {
+            bool isProgramOk = true;
+            return isProgramOk;
         }
     }
 }
+ 
